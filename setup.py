48e483b097a204a696e865ae5ad40fc68b8232f2
Simon Kelly <skelly@dimagi.com>
2014-12-11 15:28:48 +0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='fakecouch',
-    version='0.0.7',
+    version='0.0.8',
     description='Fake implementation of CouchDBKit api for testing purposes',
     author='Dimagi',
     author_email='dev@dimagi.com',
