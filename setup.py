06dee7167571722a022de7c77cbea7bfaa66b2ab
Nick Pellegrino <npellegrino@dimagi.com>
2017-11-21 10:09:37 -0500

    Revert "bump minor version"
    This reverts commit 512ebbb22d166b9ca098f661d8df2d08a9ac092b.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 
 setup(
     name='fakecouch',
-    version='0.1.0',
+    version='0.0.10',
     description='Fake implementation of CouchDBKit api for testing purposes',
     author='Dimagi',
     author_email='dev@dimagi.com',
