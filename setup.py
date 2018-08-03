34e35b2600f89bffd2a29e220b0a2989c5cd8978
Nick Pellegrino <npellegrino@dimagi.com>
2018-08-03 15:45:17 -0400

    bump version to 0.0.11

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='fakecouch',
-    version='0.0.10',
+    version='0.0.11',
     description='Fake implementation of CouchDBKit api for testing purposes',
     author='Dimagi',
     author_email='dev@dimagi.com',
